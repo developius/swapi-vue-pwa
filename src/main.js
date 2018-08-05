0666b6b707fa433505b9f3c739b3a64977827607
Finnian Anderson <get@finnian.io>
2018-08-06 09:16:06 +1200

    Fix analytics
    Signed-off-by: Finnian Anderson <get@finnian.io>

--- src/main.js
+++ src/main.js
@@ -7,11 +7,17 @@ import Meta from 'vue-meta'
 import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
+const isProd = process.env.NODE_ENV === 'production'
 
 Vue.use(require('vue-moment'))
 Vue.use(Meta)
 Vue.use(VueAnalytics, {
-  id: 'UA-73944565-5'
+  id: 'UA-73944565-5',
+  router,
+  debug: {
+    enabled: !isProd,
+    sendHitTask: isProd
+  }
 })
 
 /* eslint-disable no-new */
